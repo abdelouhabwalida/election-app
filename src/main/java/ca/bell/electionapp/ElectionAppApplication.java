2c5b4be298ac67b827cc8c730d4b405356a2036f
Walida <abdelouhab.walida@gmail.com>
2023-11-25 07:40:21 -0500

    ajouter des commentaires

--- src/main/java/ca/bell/electionapp/ElectionAppApplication.java
+++ src/main/java/ca/bell/electionapp/ElectionAppApplication.java
@@ -10,18 +10,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
 
+/**
+ * La classe principale de l'application Election App
+ * Cette classe est annotée avec {@link SpringBootApplication} pour activer les fonctionnalités de Spring
+ * Elle implémente également {@link CommandLineRunner} pour exécuter du code après le chargement du contexte de l'application
+ */
 @SpringBootApplication
 public class ElectionAppApplication implements CommandLineRunner {
     Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     ElectionHandler electionHandler;
 
+    /**
+     * La méthode principale pour démarrer l'application Spring Boot
+     *
+     * @param args Les arguments de la ligne de commande
+     */
     public static void main(String[] args) {
         SpringApplication.run(ElectionAppApplication.class, args);
     }
 
+    /**
+     * Cette méthode est exécutée après le chargement du contexte de l'application
+     * Elle traite l'élection, affiche les principaux candidats et gère les exceptions potentielles
+     *
+     * @param args Les arguments de la ligne de commande
+     */
     @Override
-    public void run(String... args)  {
+    public void run(String... args) {
 
         List<String> topCandidates;
         List<String> fraudMessage;
@@ -31,10 +47,10 @@ public class ElectionAppApplication implements CommandLineRunner {
             fraudMessage = electionHandler.displayFraudList();
 
             System.out.println(topCandidates);
-            if(!fraudMessage.isEmpty())
-            System.out.println(fraudMessage);
+            if (!fraudMessage.isEmpty())
+                System.out.println(fraudMessage);
         } catch (Exception e) {
-            logger.error("Error running the application: " , e);
+            logger.error("Error running the application: ", e);
         }
     }
 }
