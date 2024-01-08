bc37ad89e74aeec2f6257f28fc22e31d1f4727eb
Walida Abdelouhab <59826690+abdelouhabwalida@users.noreply.github.com>
2024-01-08 09:13:37 -0500

    Update ElectionAppApplication.java

--- src/main/java/ca/bell/electionapp/ElectionAppApplication.java
+++ src/main/java/ca/bell/electionapp/ElectionAppApplication.java
@@ -11,8 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 /**
-
- * La classe principale de l'application Election App
+  
+ * cette version et la version 1 -**La classe principale de l'application Election App
  * Cette classe est annotée avec {@link SpringBootApplication} pour activer les fonctionnalités de Spring
  * Elle implémente également {@link CommandLineRunner} pour exécuter du code après le chargement du contexte de l'application  ttt
  */
